35e52d6426b8b3ca4f7e953f690d93867ea43711
jpprogramerdev <jpgertto.1204fernadesz@gmail.com>
2023-05-28 16:03:24 -0300

    Atualizado 28/05/2023

--- Scripts/loadImg.js
+++ Scripts/loadImg.js
@@ -1,6 +1,6 @@
-const imagem = document.getElementById("imgUser")
-const input = document.getElementById("upImgUser")
-  
-input.addEventListener("change", () =>{
-  imagem.src = URL.createObjectURL(input.files[0])
-})
+const imagem = document.getElementById("imgUser");
+const input = document.querySelector("input[name='imagem']");
+
+input.addEventListener("change", () => {
+  imagem.src = URL.createObjectURL(input.files[0]);
+});
