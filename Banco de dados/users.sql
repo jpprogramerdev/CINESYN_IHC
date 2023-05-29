f2afc8011d56327def50c9eb7938e7f766cc9e59
jpprogramerdev <jpgertto.1204fernadesz@gmail.com>
2023-05-29 20:40:05 -0300

    finalizado dia 29/05/2023

--- Banco de dados/users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user_CINESYN(
-    id_user INT AUTO_INCREMENT PRIMARY KEY,
-    nome_user VARCHAR(150) NOT NULL,
-    email_user VARCHAR(100) NOT NULL,
-    senha_user VARCHAR(100) NOT NULL,
-    tipo_user ENUM('user', 'adm') NOT NULL,
-    img_user BLOB
-);
